d95e2a9b3d61ae2098b33a2523fee7f910f548a3
Gurjot K <147576916+gurj096@users.noreply.github.com>
2023-10-11 10:03:57 -0700

    Update NicknameBot.py
    Added a while loop and a try/except to catch instances of a name being entered that does not match the name of a variable in the .yml file

--- NicknameBot.py
+++ NicknameBot.py
@@ -18,11 +18,16 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 @bot.command()
 async def nickname(ctx): #Defines the wake command as '/nickname'. Once entered, the bot will respond with "What is your name"
     await ctx.send('What is your name')
-    name = await bot.wait_for('message') #Bot waits for name from user before continuing
-    name = name.content #Converts name into readable string
-    f_letter_name = name[0].lower() #Gets the first char of string and saves it to a variable
-    adj_list = adjectives[f_letter_name] # Imports list from .yml file associated with variable name
-    random_adj = random.choice(adj_list) # Randomly selects one element from list
-    await ctx.send(f'Your new nickname is {random_adj.title()} {name.title()}') #Outputs new nickname
+    while True: #Loop to prompt the user to enter a different name if first char of entered name is not a key in the dictionary
+        name = await bot.wait_for('message') #Bot waits for name from user before continuing
+        name = name.content #Converts name into readable string
+        f_letter_name = name[0].lower() #Gets the first char of string and saves it to a variable
+        try: #Tests whether an error will be thrown or not depending on if the f_letter_name variable is a key 
+            adj_list = adjectives[f_letter_name] # Imports list from .yml file associated with variable name
+            random_adj = random.choice(adj_list) # Randomly selects one element from list
+            await ctx.send(f'Your new nickname is {random_adj.title()} {name.title()}') #Outputs new nickname
+            break
+        except: #If error is thrown, will prompt user to enter a valid name 
+            await ctx.send('Please enter a valid name')
 
 bot.run() # This would normally include unique key provided by Discord
